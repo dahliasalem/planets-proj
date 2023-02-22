740db1bd10d7e5201bcd86bdf76c753c03116927
Dahlia Salem <admin@Admins-MacBook-Pro.local>
2023-02-21 22:58:41 -0800

    deleted unneccasry files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import PlanetVue from "@/components/Planet.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
